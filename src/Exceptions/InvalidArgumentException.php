c294372de9b2f9cb7919943e01940e7fe36039ff
RockyHuas <1169078896@qq.com>
2019-11-09 03:16:03 +0000

    Apply fixes from StyleCI

--- src/Exceptions/InvalidArgumentException.php
+++ src/Exceptions/InvalidArgumentException.php
@@ -1,15 +1,16 @@
 <?php
-/**
- * Created by RockyHuas.
- * User: Huangzb
- * Date: 2019/11/8
- * Time: 16:57
+
+/*
+ * This file is part of the rocky-huas/weather.
+ *
+ * (c) RockyHuas <1169078896@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace RockyHuas\Weather\Exceptions;
 
-
 class InvalidArgumentException extends Exception
 {
-
 }
